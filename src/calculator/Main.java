c03482de622f2097932c09f4c60fe6e1f369d3d2
raptor4694 <raptor4694@gmail.com>
2018-06-26 18:36:03 -0400

    Added option to set directory via command line arguments

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -2,7 +2,9 @@ package calculator;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		
+		if (args.length != 0) {
+			System.setProperty("user.dir", args[0]);
+		}
 		try {
 			Console.run();
 		} catch (Throwable t) {
